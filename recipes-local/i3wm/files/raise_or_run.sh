fac6291d75d902acd24dcf156090f621b6d8895c
MarkusVolk <f_l_k@t-online.de>
2017-08-26 07:00:12 +0200

    revert changes to raise_or_run. wmctrl seems to be only partial compatible with i3.
    not all active windows could be listed

--- recipes-local/i3wm/files/raise_or_run.sh
+++ recipes-local/i3wm/files/raise_or_run.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-
-#usually the window class is named like the binary
-class=$1
-
-#define exceptions for programs that are not following unix naming standards
-if [ $1 = "subl" ];then
-	class="Sublime_text"
-fi
-
-#if program is running, give focus
-if `wmctrl -xa $1`;then
-	i3-msg [class=$class] focus
-else
-#else launch
-	exec dbus-launch $1
+case "$1" in
+	"class" )
+		count=`xdotool search --class "$2" | wc -l`
+		i3wm_criteria="class";;
+	"instance" )
+		count=`xdotool search --classname "$2" | wc -l`
+		i3wm_criteria="instance";;
+	"name" )
+		count=`xdotool search --name "$2" | wc -l`
+		i3wm_criteria="title";;
+esac
+if [ "$count" -eq 0 ]; then
+	dbus-launch $3
 fi
